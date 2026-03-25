47aef04e28e18094ed8ea31e82992c67a6fd73b1
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-25 14:51:23 +0100

    fix: fix shop main

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,9 +2,7 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Carrello carrello1 = new Carrello();
 
-        carrello1.addItem();
     }
 
 }
